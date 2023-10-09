7ac055dd134a644dc1107340e3378323f91fde1e
Gsousaz <doomlighthighlord@gmail.com>
2023-10-09 03:18:19 -0300

    Fix: Function call correction on bookings-controller

--- src/controllers/bookings-controller.ts
+++ src/controllers/bookings-controller.ts
@@ -6,9 +6,8 @@ import httpStatus from "http-status";
 async function showBooking(req: AuthenticatedRequest, res: Response) {
     const { userId } = req;
 
-    const showBooking = await bookingsService.getBookingUser(userId);
-
-    return res.status(httpStatus.OK).send(showBooking);
+    const searchedBooking = await bookingsService.getBookingUser(userId);
+    res.status(httpStatus.OK).send(searchedBooking);
 }
 
 
